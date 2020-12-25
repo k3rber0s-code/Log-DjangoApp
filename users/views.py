856d4345dfc33d0b079b5a094dee8e40500c5620
ema <tomanovaema@gmail.com>
2020-12-25 15:01:28 +0100

    First commit

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        #Display blank registration form.
+        form = UserCreationForm()
+    else:
+        #Process completed forms
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the new user in and redirect to home page.
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    #Display blank or invalid forms
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
